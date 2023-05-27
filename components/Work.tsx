d823b29f218b730a08591d8afb4e099e27489c7a
jayjunn <jayjunn@outlook.com>
2023-05-27 12:21:43 +0900

    feat  works data update

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Image from "next/image";
-import styles from "../styles/Work.module.css";
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/Work.module.css';
 
 type Work = {
   name: string;
@@ -12,21 +12,30 @@ type Work = {
 
 const Work = (props: Work) => {
   const { name, img, description, stacks, link } = props;
+  const imageStyle = {
+    borderRadius: '10px',
+  };
   return (
     <li className={styles.project__container}>
       <span className={styles.project__name}>{name}</span>
-      <div className={styles.project__img}>
+      {link ? (
         <a href={link} target="_blank" rel="noreferrer">
-          <Image src={img} alt="project image" width={1000} height={750} />
+          <button className={styles.project__img__container}>
+            <Image src={img} alt="project__image" className="project__image" width={'1000'} height={750} loading="lazy" />
+          </button>
         </a>
-      </div>
+      ) : (
+        <div className={styles.project__img__container}>
+          <Image src={img} alt="project__image" className="project__image" width={'1000'} height={750} style={imageStyle} />
+        </div>
+      )}
       <div className={styles.project__details}>
         <p className={styles.project__des}>{description}</p>
         <p className={styles.project__stacks}>
           {stacks.map((i, index) => {
             return (
               <span key={index}>
-                {i} {index === stacks.length - 1 ? "" : "/ "}
+                {i} {index === stacks.length - 1 ? '' : '/ '}
               </span>
             );
           })}
--- data.js
+++ data.js
@@ -1,5 +1,30 @@
 const data = [
   {
+    id: 5,
+    name: `SILVER MINE`,
+    img: `https://res.cloudinary.com/dgmnoyv6u/image/upload/v1685156087/Screenshot_2023-05-27_at_11.54.27_AM_qviyp4.png`,
+    description: `Token-issuing SaaS for a wide range of companies preparing for an open securities token (STO) market`,
+    stacks: [`REACT`, `TYPESCRIPT`, `REACT QUERY`, `SCSS`],
+    link: `https://beta.silvermine.me/`,
+  },
+
+  {
+    id: 4,
+    name: `COS`,
+    img: `https://res.cloudinary.com/dgmnoyv6u/image/upload/v1685155499/Screenshot_2023-05-27_at_11.44.37_AM_u8no2c.png`,
+    description: `A global fashion ecommerce project.`,
+    stacks: [`JAVASCRIPT`, `CSS`, 'HTML', 'ADOBE'],
+    link: `https://www.cos.com/`,
+  },
+  {
+    id: 5,
+    name: `CCCV NFT`,
+    img: `https://res.cloudinary.com/dgmnoyv6u/image/upload/v1685157442/Screenshot_2023-05-27_at_12.16.55_PM-removebg-preview_zygl4p.png`,
+    description: `A platform that allows you to conveniently issue NFTs without installing a separate wallet`,
+    stacks: [`VUE.JS`, `VUEX`, `SCSS`],
+    link: `https://cccv.to/nft`,
+  },
+  {
     id: 0,
     name: `humble bank`,
     img: `https://res.cloudinary.com/dgmnoyv6u/image/upload/v1654311191/portfolio/humble_bank_ekxbro.png`,
@@ -7,26 +32,18 @@ const data = [
     stacks: [`REACT NATIVE`, `EXPO`, `EXPRESS`, `MONGODB`],
     link: `https://github.com/jayjunn/humble-bank`,
   },
+
   {
     id: 1,
-
     name: `glue`,
     img: `https://res.cloudinary.com/dgmnoyv6u/image/upload/v1654311191/portfolio/glue_zdfvfz.png`,
     description: `Glue is a knowledge marketplace where you can find interesting and smart people with a skill to offer.`,
     stacks: [`REACT`, `TYPESCRIPT`, `NEXT.JS`, `GRAPH QL`, `POSTGRES QL`],
-    link: `https://github.com/jayjunn/`,
+    link: ``,
   },
   {
     id: 2,
-    name: `feedback`,
-    img: `https://res.cloudinary.com/dgmnoyv6u/image/upload/v1654311190/portfolio/feedback_jtx4kl.png`,
-    description: `A web platform where customers can leave reviews with star rating system.`,
-    stacks: [`REACT`, `TYPESCRIPT`],
-    link: `https://github.com/jayjunn/Customer-feedback`,
-  },
-  {
-    id: 2,
-    name: `feedback`,
+    name: `FeedBack`,
     img: `https://res.cloudinary.com/dgmnoyv6u/image/upload/v1654311190/portfolio/feedback_jtx4kl.png`,
     description: `A web platform where customers can leave reviews with star rating system.`,
     stacks: [`REACT`, `TYPESCRIPT`],
