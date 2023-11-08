46228a69737e6574f7aa16d28317ad8c48c46f38
jayjunn <jayjunn@outlook.com>
2023-11-08 11:18:58 +0900

    fix: add og metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,14 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Younggeun Jun</title>
-        <meta name="description" content="Younggeun Jun" />
+        <meta
+          name="description"
+          content="Younggeun Jun is a front-end developer who builds accessible, inclusive products and digital experiences for the web."
+        />
+        <meta property="og:title" content="Younggeun Jun" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://younggeun-jun.netlify.app" />
+        <meta property="og:image" content="https://res.cloudinary.com/dgmnoyv6u/image/upload/c_thumb,w_200,g_face/v1699409874/og_ugafxm.jpg"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.contents}>
